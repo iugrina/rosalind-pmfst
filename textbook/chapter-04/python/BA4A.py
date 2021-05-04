d5203fa0a7279a7c5c6aa2b50a363da9047951cf
Ivo Ugrina <ivougrina@gmail.com>
2021-05-04 19:01:12 +0200

    add BA4A.py

---
+++ textbook/chapter-04/python/BA4A.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+"""
+A solution to a Rosalind bioinformatics problem
+
+Problem Title: Translate an RNA String into an Amino Acid String
+Rosalind ID: BA4A
+URL: http://rosalind.info/problems/ba4a/
+"""
+
+
+def get_genetic_code_codons():
+    genetic_code = {
+        "AAA": "K",
+        "AAC": "N",
+        "AAG": "K",
+        "AAU": "N",
+        "ACA": "T",
+        "ACC": "T",
+        "ACG": "T",
+        "ACU": "T",
+        "AGA": "R",
+        "AGC": "S",
+        "AGG": "R",
+        "AGU": "S",
+        "AUA": "I",
+        "AUC": "I",
+        "AUG": "M",
+        "AUU": "I",
+        "CAA": "Q",
+        "CAC": "H",
+        "CAG": "Q",
+        "CAU": "H",
+        "CCA": "P",
+        "CCC": "P",
+        "CCG": "P",
+        "CCU": "P",
+        "CGA": "R",
+        "CGC": "R",
+        "CGG": "R",
+        "CGU": "R",
+        "CUA": "L",
+        "CUC": "L",
+        "CUG": "L",
+        "CUU": "L",
+        "GAA": "E",
+        "GAC": "D",
+        "GAG": "E",
+        "GAU": "D",
+        "GCA": "A",
+        "GCC": "A",
+        "GCG": "A",
+        "GCU": "A",
+        "GGA": "G",
+        "GGC": "G",
+        "GGG": "G",
+        "GGU": "G",
+        "GUA": "V",
+        "GUC": "V",
+        "GUG": "V",
+        "GUU": "V",
+        "UAA": "*",
+        "UAC": "Y",
+        "UAG": "*",
+        "UAU": "Y",
+        "UCA": "S",
+        "UCC": "S",
+        "UCG": "S",
+        "UCU": "S",
+        "UGA": "*",
+        "UGC": "C",
+        "UGG": "W",
+        "UGU": "C",
+        "UUA": "L",
+        "UUC": "F",
+        "UUG": "L",
+        "UUU": "F",
+    }
+
+    return genetic_code
+
+
+def translation(rna_pattern):
+    G = get_genetic_code_codons()
+
+    res = []
+    for i in range(0, len(rna_pattern), 3):
+        letter = G[rna_pattern[i : i + 3]]
+        if letter == "*":
+            break
+        res.append(letter)
+
+    res = "".join(res)
+
+    return res
+
+
+if __name__ == "__main__":
+
+    import sys
+
+    inlines = [x.strip("\n") for x in sys.stdin.readlines()]
+    text = inlines[0]
+
+    res = translation(text)
+
+    sys.stdout.write(res)
