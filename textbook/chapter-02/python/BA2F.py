50f38f292c84f545df966bc1d8e8f1d9e591fa23
Ivo Ugrina <ivougrina@gmail.com>
2022-03-21 17:06:49 +0100

    remove clutter from BA2F

--- textbook/chapter-02/python/BA2F.py
+++ textbook/chapter-02/python/BA2F.py
@@ -29,34 +29,6 @@ def patternprob(pattern, profile):
     return p
 
 
-def kmersindices(text, k):
-    """Find indices of all k-mers in text"""
-    D = dict()
-    for i in range(0, len(text) - k + 1):
-        tmp = kmer(text, i, k)
-        try:
-            D[tmp].append(i)
-        except KeyError:
-            D[tmp] = [i]
-    return D
-
-
-def kmersfrequency(text, k):
-    """Find the number of occurrences of all k-mers in text"""
-    D = kmersindices(text, k)
-    for k, v in D.items():
-        D[k] = len(v)
-    return D
-
-
-def patternprob(pattern, profile):
-    """return the probability of observing the pattern with the profile matrix"""
-    p = 1
-    for x in enumerate(pattern):
-        p = p * profile[x[1]][x[0]]
-    return p
-
-
 def ProfileMostProbable(text, k, profile, first=False):
     """
     Find a Profile-most probable k-mer in a string text.
@@ -64,20 +36,12 @@ def ProfileMostProbable(text, k, profile, first=False):
     """
     maxkey = ""
     maxp = -1
-    if first:
-        for end in range(k, len(text) + 1):
-            key = text[end - k : end]
-            tmp = patternprob(key, profile)
-            if tmp > maxp:
-                maxkey = key
-                maxp = tmp
-    else:
-        D = kmersfrequency(text, k)
-        for key in D.keys():
-            tmp = patternprob(key, profile)
-            if tmp > maxp:
-                maxkey = key
-                maxp = tmp
+    for end in range(k, len(text) + 1):
+        key = text[end - k : end]
+        tmp = patternprob(key, profile)
+        if tmp > maxp:
+            maxkey = key
+            maxp = tmp
     return maxkey
 
 
